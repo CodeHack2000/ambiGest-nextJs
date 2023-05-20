f5b08ee6b9b546c29d9f669116619982b95b08af
Vladyslav <vladyslavsafronov@gmail.com>
2023-05-20 15:23:41 +0100

    Fixed Some Containers View

--- src/components/RegisterCard.tsx
+++ src/components/RegisterCard.tsx
@@ -4,7 +4,7 @@ import AuthInput from '@/ui/AuthInput'
 
 export default function RegisterCard() {
   return (
-    <div className='w-full mx-8 customMd:w-1/2 shadow-md shadow-black grid grid-cols-1 justify-center px-10 rounded-xl lg:w-2/5'>
+    <div className='w-full mx-8 customMd:w-1/2 shadow-md shadow-black flex flex-col justify-center px-10 rounded-xl lg:w-2/5'>
         <span className='font-semibold text-md text-left mt-6 mb-2'>Bem vindo a <span className='text-primary-dark'>ambiGest</span> !</span>
         <span className='text-sm mb-8 font-semibold'>Juntos tornamos o mundo melhor</span>
         <AuthInput mode="register" inputType='email' />
