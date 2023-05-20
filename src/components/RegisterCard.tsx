b5971eab38dd68e2ada1276374751b5f0d12794f
Vladyslav <vladyslavsafronov@gmail.com>
2023-05-20 18:04:25 +0100

    Optimized all pages for mobile

--- src/components/RegisterCard.tsx
+++ src/components/RegisterCard.tsx
@@ -6,7 +6,7 @@ export default function RegisterCard() {
   return (
     <div className='mx-8 shadow-md shadow-black flex flex-col justify-center px-10 rounded-xl customAuthCardScreen'>
         <span className='font-semibold text-md text-left mt-6 mb-2'>Bem vindo a <span className='text-primary-dark'>ambiGest</span> !</span>
-        <span className='text-sm mb-8 font-semibold'>Juntos tornamos o mundo melhor</span>
+        <span className='text-sm mb-3 font-semibold'>Juntos tornamos o mundo melhor</span>
         <AuthInput mode="register" inputType='email' />
         <AuthInput mode="register" inputType='password' />
         <AuthButton mode="register" />
