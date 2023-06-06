1261ca2657a2a5c88a533333d1cdf290358eeef8
Vladyslav <vladyslavsafronov@gmail.com>
2023-06-06 23:24:58 +0100

    Add invoice page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
-
+// changed
 export default function Navbar() {
   return (
     <div className='w-full h-9 bg-primary-dark shadow-sm shadow-black flex justify-evenly items-center pt-1 px-32' >
